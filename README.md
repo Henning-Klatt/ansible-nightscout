acfc73c8a8883ea827f054a8261fabed02c09c7b
Henning-Klatt <henning.klatt@web.de>
2023-04-25 15:16:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ansible-nightscout
+
+## WiP!
+
+### install ansible community collection
+```
+ansible-galaxy collection install community.general
+```
+
+### run ansible playbook
+```
+ansible-playbook -i inventory.ini nightscout.yaml
+```
---
+++ roles/nightscout/files/start.sh
@@ -0,0 +1,16 @@
+# environment variables
+export DISPLAY_UNITS="mg/dl"
+export MONGO_CONNECTION="mongodb://MONGO_NS_USER:MONGO_NS_PASSWORD@localhost:27017/MONGO_NS_DB"
+export DBSIZE_MAX=6000
+export INSECURE_USE_HTTP=true
+export PORT=47342
+export API_SECRET=""
+export PUMP_FIELDS="reservoir battery status"
+export DEVICESTATUS_ADVANCED=true
+export ENABLE="careportal iob cob openaps pump bwg rawbg basal"
+export TIME_FORMAT=24
+export THEME="colors"
+
+# start server
+node server.js
+
